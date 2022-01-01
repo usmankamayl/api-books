83f2ad488a2795bdd1a5f38e6ee9ceffdff11501
usmankamayl <usman.kamailaev@yandex.ru>
2022-01-01 21:35:24 +0300

    mongo

--- index.js
+++ index.js
@@ -26,13 +26,13 @@ const UserDB = process.env.DB_USERNAME || 'root';
 const PasswordDB = process.env.DB_PASSWORD || 'qwerty12345';
 const NameDB = process.env.DB_NAME || 'books'
 const HostDb = process.env.DB_HOST || 'mongodb://localhost:27017/'
-async function start() {
+async function start(uri, callback) {
     try {
         //const UrlDB = `mongodb+srv://${UserDB}:${PasswordDB}@cluster0.grfrs.mongodb.net/${NameDB}`;
         //const UrlDB = `mongodb://localhost:27017/mydb`;
         //const UrlDB = `mongodb://${UserDB}:${PasswordDB}@localhost:27017/mydb`;
         //await mongoose.connect(UrlDb);
-        
+
         await mongoose.connect(HostDb, {
             user: UserDB,
             pass: PasswordDB,
@@ -48,6 +48,5 @@ async function start() {
         console.log(e);
     }
 }
-
 
 start();
